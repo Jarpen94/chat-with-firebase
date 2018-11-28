38161910a169d41d25d86c251d7ef92e9d647c8b
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-28 11:17:27 +0100

    created firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyCDQPim8PaszBpjfAE_AG_tJBlHZmUzXcQ",
+    authDomain: "poniedzialek-e01cf.firebaseapp.com",
+    databaseURL: "https://poniedzialek-e01cf.firebaseio.com",
+    projectId: "poniedzialek-e01cf",
+    storageBucket: "poniedzialek-e01cf.appspot.com",
+    messagingSenderId: "883759460601"
+}
+
+firebase.initializeApp(config)
+
+export const database = firebase.database()
