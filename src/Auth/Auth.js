08b7e3b5e340de6a19c5fa20231711a43e671d5f
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-29 14:29:51 +0100

    login is rememberd and saves in local storage

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Forms from './Forms'
 
+import { auth } from '../firebase'
+
 
 
 class Auth extends React.Component {
@@ -10,27 +12,40 @@ class Auth extends React.Component {
         isUserLoggedIn: false
     }
 
+    componentDidMount() {
+        auth.onAuthStateChanged(
+            user => {
+                console.log(user)
+            }
+        )
+    }
+
     UserHandler = event => this.setState({ username: event.target.value })
     PasswordHandler = event => this.setState({ password: event.target.value })
 
-    onLogClick = () => { }
+    onLogClick = () => {
+        auth.signInWithEmailAndPassword(this.state.username, this.state.password)
+            .then(console.log)
+            .catch(console.log)
+    }
+
     onLogInByGoogleCLick = () => { }
 
     render() {
         return (
             this.state.isUserLoggedIn ?
-            this.props.children
-            :
-            <Forms
-                username={this.state.username}
-                UserHandler={this.UserHandler}
-                PasswordHandler={this.PasswordHandler}
-                onLogClick={this.onLogClick}
-                onLogInByGoogleCLick={this.onLogInByGoogleCLick}
+                this.props.children
+                :
+                <Forms
+                    username={this.state.username}
+                    UserHandler={this.UserHandler}
+                    PasswordHandler={this.PasswordHandler}
+                    onLogClick={this.onLogClick}
+                    onLogInByGoogleCLick={this.onLogInByGoogleCLick}
 
 
 
-            />
+                />
 
         )
     }
